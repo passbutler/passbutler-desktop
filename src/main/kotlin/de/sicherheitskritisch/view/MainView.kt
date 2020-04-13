54ff4317360a55d8b341d56b33ea27fd6c291f07
Bastian Raschke <bastian.raschke@posteo.de>
2020-04-13 16:13:20 +0200

    Test usage of common code

--- src/main/kotlin/de/sicherheitskritisch/view/MainView.kt
+++ src/main/kotlin/de/sicherheitskritisch/view/MainView.kt
@@ -1,11 +1,18 @@
 package de.sicherheitskritisch.view
 
+import de.passbutler.common.crypto.RandomGenerator
 import de.sicherheitskritisch.app.Styles
+import kotlinx.coroutines.runBlocking
 import tornadofx.*
 
-class MainView : View("Passbutler") {
+class MainView : View("Pass Butler") {
     override val root = hbox {
-        label(title) {
+
+        val d = runBlocking {
+            RandomGenerator.generateRandomBytes(1).first().toString()
+        }
+
+        label(title + d) {
             addClass(Styles.heading)
         }
     }
