5408f241b516bb312262dfb5641087a8f4e50138
Bastian Raschke <bastian.raschke@posteo.de>
2020-10-18 14:46:46 +0200

    Working on login screen

--- src/main/kotlin/de/passbutler/desktop/LoginScreen.kt
+++ src/main/kotlin/de/passbutler/desktop/LoginScreen.kt
@@ -3,67 +3,129 @@ package de.passbutler.desktop
 import de.passbutler.desktop.ui.*
 import javafx.geometry.Orientation
 import javafx.geometry.Pos
+import javafx.scene.Group
+import javafx.scene.Node
+import javafx.scene.control.Button
 import tornadofx.*
 
 class LoginScreen : View("Login") {
 
+    override val root = hbox(alignment = Pos.CENTER)
+
+    private val viewModel: LoginViewModel by inject()
+
+    private var serverUrlField: Field? = null
+
     private var light = true
 
-    override val root = hbox(alignment = Pos.CENTER) {
+    init {
+        with(root) {
+            setupForegroundContainer()
+        }
+    }
+
+    private fun Node.setupForegroundContainer(): Group {
         addClass(BaseTheme.abstractBackgroundStyle)
 
         onLeftClick {
             requestFocus()
         }
 
-        group {
-            form {
-                addClass(BaseTheme.cardViewBackgroundStyle)
+        return group {
+            setupForm()
+        }
+    }
 
-                style {
-                    paddingAll = marginM.value
-                    minWidth = 180.pt
+    private fun Node.setupForm() {
+        form {
+            addClass(BaseTheme.cardViewBackgroundStyle)
+
+            style {
+                paddingAll = marginM.value
+                minWidth = 180.pt
+            }
+
+            label(messages["login_description"])
+
+            fieldset(labelPosition = Orientation.VERTICAL) {
+                paddingTop = marginM.value
+                paddingBottom = marginM.value
+
+                spacing = marginS.value
+
+                serverUrlField = createServerUrlField()
+
+                createUsernameUrlField()
+                createPasswordUrlField()
+                createLocalLoginCheckbox()
+            }
+
+            createLoginButton()
+        }
+    }
+
+    private fun Fieldset.createServerUrlField(): Field {
+        return field(messages["login_serverurl_hint"]) {
+            textfield(viewModel.serverUrlProperty) {
+                whenDocked {
+                    requestFocus()
                 }
 
-                label(messages["login_description"])
-
-                fieldset(labelPosition = Orientation.VERTICAL) {
-                    paddingTop = marginM.value
-                    paddingBottom = marginM.value
-
-                    spacing = marginS.value
-
-                    field(messages["login_username_hint"]) {
-                        textfield {
-                            whenDocked {
-                                requestFocus()
-                            }
-                        }
-                    }
-                    field(messages["login_master_password_hint"]) {
-                        passwordfield {
-                        }
-                    }
-                    field {
-                        checkbox(messages["login_local_login_label"])
+                validator {
+                    if (!viewModel.isLocalLoginProperty.value && it.isNullOrBlank()) {
+                        error(viewModelBundle["required"])
+                    } else {
+                        null
                     }
                 }
+            }
+        }
+    }
 
-                jfxButtonRaised(messages["login_button_text"]) {
-                    isDefaultButton = true
+    private fun Fieldset.createUsernameUrlField(): Field {
+        return field(messages["login_username_hint"]) {
+            textfield(viewModel.usernameProperty) {
+                required()
+            }
+        }
+    }
 
-                    action {
-                        println("Login pressed light = $light")
+    private fun Fieldset.createPasswordUrlField(): Field {
+        return field(messages["login_master_password_hint"]) {
+            passwordfield(viewModel.passwordProperty) {
+                required()
+            }
+        }
+    }
 
-                        if (light) {
-                            ThemeManager.changeTheme(Theme.DARK)
-                        } else {
-                            ThemeManager.changeTheme(Theme.LIGHT)
-                        }
-
-                        light = !light
-                    }
+    private fun Fieldset.createLocalLoginCheckbox(): Field {
+        return field {
+            checkbox(messages["login_local_login_label"], viewModel.isLocalLoginProperty) {
+                action {
+                    serverUrlField?.isVisible = !viewModel.isLocalLoginProperty.value
                 }
+            }
+        }
+    }
+
+    private fun Form.createLoginButton(): Button {
+        return jfxButtonRaised(messages["login_button_text"]) {
+            isDefaultButton = true
+
+            action {
+                viewModel.validate()
+
+                if (viewModel.valid.value) {
+                    // TODO: Start request sending with viewModel.loginUser()
+                }
+
+                if (light) {
+                    ThemeManager.changeTheme(Theme.DARK)
+                } else {
+                    ThemeManager.changeTheme(Theme.LIGHT)
+                }
+
+                light = !light
             }
         }
     }
---
+++ src/main/kotlin/de/passbutler/desktop/LoginViewModel.kt
@@ -0,0 +1,22 @@
+package de.passbutler.desktop
+
+import de.passbutler.common.base.Result
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.ViewModel
+
+class LoginViewModel : ViewModel() {
+    val serverUrlProperty = bind { SimpleStringProperty() }
+    val usernameProperty = bind { SimpleStringProperty() }
+    val passwordProperty = bind { SimpleStringProperty() }
+    val isLocalLoginProperty = bind { SimpleBooleanProperty() }
+
+    suspend fun loginUser(serverUrlString: String?, username: String, masterPassword: String): Result<Unit> {
+        val userManager = PassButlerApplication.userManager
+
+        return when (serverUrlString) {
+            null -> userManager.loginLocalUser(username, masterPassword)
+            else -> userManager.loginRemoteUser(username, masterPassword, serverUrlString)
+        }
+    }
+}
