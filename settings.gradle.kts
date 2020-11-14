68f449fed0185765e03293a689fdcb8146849910
Bastian Raschke <bastian.raschke@posteo.de>
2020-11-14 13:55:18 +0100

    Using common module with Kotlin DSL build script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
 rootProject.name = "Pass Butler Desktop"
 
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.squareup.sqldelight") {
+                useModule("com.squareup.sqldelight:gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
 include(":PassButlerCommon")
