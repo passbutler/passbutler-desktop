eed16ee150d80a993ed01398ef81478ce751a511
Bastian Raschke <bastian.raschke@posteo.de>
2020-11-29 12:28:49 +0100

    Added synchronization UI

--- src/main/kotlin/de/passbutler/desktop/OverviewScreen.kt
+++ src/main/kotlin/de/passbutler/desktop/OverviewScreen.kt
@@ -1,22 +1,48 @@
 package de.passbutler.desktop
 
+import com.jfoenix.controls.JFXSpinner
+import de.passbutler.common.Webservices
+import de.passbutler.common.base.BindableObserver
+import de.passbutler.common.ui.RequestSending
+import de.passbutler.common.ui.launchRequestSending
 import de.passbutler.desktop.ui.NavigationMenuScreen
+import de.passbutler.desktop.ui.Theme
 import de.passbutler.desktop.ui.injectWithPrivateScope
+import de.passbutler.desktop.ui.jfxButtonRaised
+import de.passbutler.desktop.ui.jfxSpinner
 import de.passbutler.desktop.ui.marginM
 import de.passbutler.desktop.ui.marginS
+import de.passbutler.desktop.ui.showFadeInOutAnimation
 import de.passbutler.desktop.ui.textLabelBody1
 import de.passbutler.desktop.ui.textLabelHeadline
 import javafx.geometry.Pos
 import javafx.scene.Node
+import javafx.scene.layout.Pane
+import kotlinx.coroutines.Job
+import org.tinylog.kotlin.Logger
 import tornadofx.FX.Companion.messages
+import tornadofx.action
+import tornadofx.addClass
 import tornadofx.get
+import tornadofx.hbox
 import tornadofx.paddingAll
+import tornadofx.paddingBottom
 import tornadofx.paddingTop
 import tornadofx.vbox
 
-class OverviewScreen : NavigationMenuScreen(messages["overview_title"]) {
+class OverviewScreen : NavigationMenuScreen(messages["overview_title"]), RequestSending {
 
     private val viewModel by injectWithPrivateScope<OverviewViewModel>()
+
+    private var refreshSpinner: JFXSpinner? = null
+
+    private var synchronizeDataRequestSendingJob: Job? = null
+
+    private val webservicesInitializedObserver: BindableObserver<Webservices?> = {
+        if (it != null) {
+            synchronizeData(userTriggered = false)
+        }
+    }
 
     override fun Node.createMainContent() {
         vbox {
@@ -26,8 +52,59 @@ class OverviewScreen : NavigationMenuScreen(messages["overview_title"]) {
             textLabelHeadline(messages["overview_empty_screen_title"])
             textLabelBody1(messages["overview_empty_screen_description"]) {
                 paddingTop = marginS.value
+                paddingBottom = marginM.value
+            }
+
+            // TODO: Proper UI
+            jfxButtonRaised("Synchronize") {
+                action {
+                    synchronizeData(userTriggered = true)
+                }
+            }
+
+            refreshSpinner = jfxSpinner() {
+                isVisible = false
             }
         }
     }
-}
 
+    override fun onDock() {
+        super.onDock()
+
+        viewModel.loggedInUserViewModel?.webservices?.addObserver(this, true, webservicesInitializedObserver)
+    }
+
+    override fun onUndock() {
+        viewModel.loggedInUserViewModel?.webservices?.removeObserver(webservicesInitializedObserver)
+
+        super.onUndock()
+    }
+
+    private fun synchronizeData(userTriggered: Boolean) {
+        val synchronizeDataRequestRunning = synchronizeDataRequestSendingJob?.isActive ?: false
+
+        if (!synchronizeDataRequestRunning) {
+            synchronizeDataRequestSendingJob = launchRequestSending(
+                handleSuccess = {
+                    // Only show user feedback if it was user triggered to avoid confusing the user
+                    if (userTriggered) {
+                        showInformation(messages["overview_sync_successful_message"])
+                    }
+                },
+                handleFailure = {
+                    // Only show user feedback if it was user triggered to avoid confusing the user
+                    if (userTriggered) {
+                        showError(messages["overview_sync_failed_message"])
+                    }
+                },
+                handleLoadingChanged = { isLoading ->
+                    refreshSpinner?.showFadeInOutAnimation(isLoading)
+                }
+            ) {
+                viewModel.synchronizeData()
+            }
+        } else {
+            Logger.debug("The synchronize data request is already running - skip call")
+        }
+    }
+}
--- src/main/kotlin/de/passbutler/desktop/OverviewViewModel.kt
+++ src/main/kotlin/de/passbutler/desktop/OverviewViewModel.kt
@@ -1,5 +1,14 @@
 package de.passbutler.desktop
 
+import de.passbutler.common.LoggedInUserViewModelUninitializedException
+import de.passbutler.common.base.Result
 import tornadofx.ViewModel
 
-class OverviewViewModel : ViewModel()
+class OverviewViewModel : ViewModel(), UserViewModelUsingViewModel {
+    override val userViewModelProvidingViewModel by injectUserViewModelProvidingViewModel()
+
+    suspend fun synchronizeData(): Result<Unit> {
+        val loggedInUserViewModel = loggedInUserViewModel ?: throw LoggedInUserViewModelUninitializedException
+        return loggedInUserViewModel.synchronizeData()
+    }
+}
