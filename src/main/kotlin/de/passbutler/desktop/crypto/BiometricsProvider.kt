503ef080d0bdb5f7fe44dd5f25b35464bae39bdf
Bastian Raschke <bastian.raschke@posteo.de>
2020-12-18 22:50:55 +0100

    Return `Failure` with dedicated exception instead of crashing

--- src/main/kotlin/de/passbutler/desktop/crypto/BiometricsProvider.kt
+++ src/main/kotlin/de/passbutler/desktop/crypto/BiometricsProvider.kt
@@ -1,5 +1,6 @@
 package de.passbutler.desktop.crypto
 
+import de.passbutler.common.base.Failure
 import de.passbutler.common.base.Result
 import de.passbutler.common.crypto.BiometricsProviding
 import javax.crypto.Cipher
@@ -12,30 +13,32 @@ class BiometricsProvider : BiometricsProviding {
         }
 
     override fun obtainKeyInstance(): Result<Cipher> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun generateKey(keyName: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun initializeKeyForEncryption(keyName: String, encryptionCipher: Cipher): Result<Unit> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun initializeKeyForDecryption(keyName: String, decryptionCipher: Cipher, initializationVector: ByteArray): Result<Unit> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun encryptData(encryptionCipher: Cipher, data: ByteArray): Result<ByteArray> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun decryptData(decryptionCipher: Cipher, data: ByteArray): Result<ByteArray> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
 
     override suspend fun removeKey(keyName: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return Failure(NotYetImplementedException)
     }
+
+    object NotYetImplementedException : UnsupportedOperationException("Not yet implemented!")
 }
