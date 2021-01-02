7fb8e678ca1e7ccfe721213f0fb51e6f11a142e1
Bastian Raschke <bastian.raschke@posteo.de>
2021-01-02 18:47:54 +0100

    Added missed return value

--- src/main/kotlin/de/passbutler/desktop/ui/CommonViews.kt
+++ src/main/kotlin/de/passbutler/desktop/ui/CommonViews.kt
@@ -2,9 +2,9 @@ package de.passbutler.desktop.ui
 
 import javafx.geometry.Pos
 import javafx.scene.Node
+import javafx.scene.control.Button
 import javafx.scene.text.TextAlignment
 import tornadofx.FX
-import tornadofx.action
 import tornadofx.addClass
 import tornadofx.get
 import tornadofx.paddingAll
@@ -33,8 +33,8 @@ fun Node.createEmptyScreen(title: String, description: String): Node {
     }
 }
 
-fun Node.createCancelButton(onAction: () -> Unit) {
-    jfxButtonRaised(FX.messages["general_cancel"]) {
+fun Node.createCancelButton(onAction: () -> Unit): Button {
+    return jfxButtonRaised(FX.messages["general_cancel"]) {
         addClass(Theme.secondaryButtonStyle)
 
         setOnAction {
